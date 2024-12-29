bc47b25509eaab77bdd3cdce739c7802dd2f0953
femyfede <156244790+femyfede@users.noreply.github.com>
2024-12-28 21:47:46 -0800

    changes

--- commuinity/urls.py
+++ commuinity/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
+from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
-    path('courses/<int:course_id>/payment/', views.payment_view, name='payment'),
+    path('', lambda request: redirect('course_list'), name='home'),  # Redirect to courses
     path('courses/', views.course_list, name='course_list'),
     path('courses/<int:pk>/', views.course_detail, name='course_detail'),
     path('courses/<int:pk>/payment/', views.payment_view, name='payment_view'),
-    path('payment-success/', views.payment_success, name='payment_success'),
-
+    path('courses/payment_success/', views.payment_success, name='payment_success'),
 ]
